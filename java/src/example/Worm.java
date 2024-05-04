2a6ae0a950370cd5ce160149134522d3bae64604
Yasmin Ismail <C22485282@mytudublin.ie>
2024-05-04 21:18:27 +0100

    delcared MyVisual mv variable to access methods and properties of the parent. Worm parameters created. Constructor for the Worm class, taking a MyVisual instance as input.

--- java/src/example/Worm.java
+++ java/src/example/Worm.java
@@ -1,68 +1,28 @@
 package example;
 
 public class Worm {
-    MyVisual mv;
-    int xspacing = 8;   // How far apart should each horizontal location be spaced
-    int w;              // Width of entire wave
-    int maxwaves = 4;   // total # of waves to add together
 
-    float theta = 0.0f;
-    float[] amplitude = new float[maxwaves];   // Height of wave
-    float[] dx = new float[maxwaves];          // Value for incrementing X, to be calculated as a function of period and xspacing
-    float[] yvalues;                           // Using an array to store height values for the wave (not entirely necessary)
-
-    public Worm(MyVisual mv) {
-        this.mv = mv;
-        w = mv.width + 16; // Moved initialization here
-        
-        for (int i = 0; i < maxwaves; i++) {
-            amplitude[i] = mv.random(100,120);
-            float period = mv.random(300,1000); // How many pixels before the wave repeats
-            dx[i] = (mv.TWO_PI / period) * xspacing;
-        }
-
-        yvalues = new float[w/xspacing];
-    }
-
-    public void render() {
-        mv.hint(mv.DISABLE_DEPTH_TEST);
-        calcWave();
-        renderWave();
-        mv.hint(mv.ENABLE_DEPTH_TEST);
-    }
-    void calcWave() {
-        // Increment theta (try different values for 'angular velocity' here
-        theta += 0.02;
+        MyVisual mv;
+        int xspacing = 8;   // Distance of each horizontal location 
+        int w;              // Width of entire worm
+        int maxworms = 2;   // Amount of worms to add together
     
-        // Accumulate wave height values
-        for (int i = 0; i < yvalues.length; i++) {
-            float x = theta + (i * dx[0] / xspacing);
-            float waveHeight = 0;
-            for (int j = 0; j < maxwaves; j++) {
-                // Every other wave is cosine instead of sine
-                if (j % 2 == 0)  waveHeight += mv.sin(x)*amplitude[j];
-                else waveHeight += mv.cos(x)*amplitude[j];
-                x+=dx[j];
-            }
-            yvalues[i] = waveHeight;
-        }
-    }
-
-    void renderWave() {
-        // A simple way to draw the wave with an ellipse at each location
-        mv.stroke(0);
-        mv.strokeWeight(4);
-        // Smoothly change colors
-        float hue = 0;
-        float hueIncrement = 255.0f / yvalues.length;
-        for (int x = 0; x < yvalues.length; x++) {
-            float y = mv.height / 2 + yvalues[x];
-            float y2 = mv.height / 2 - yvalues[x];
-            mv.fill(hue, 255, 255);
-            mv.ellipse(x * xspacing, y, 100, 50);
-            mv.fill((hue %255)-50, 255, 255);
-            mv.ellipse(x * xspacing, y2, 65, 50); // Draw ellipse for second wave
-            hue += hueIncrement;
-        }
-    }
+        float theta = 0.0f; // Angle initialized for wave oscillation
+        float[] amplitude = new float[maxworms];   // Stores height of worm
+        float[] dx = new float[maxworms];          // Value for incrementing X
+        float[] yvalues;                           // Stores height values of the worm 
+    
+        // Constructor for the Worm class, taking a MyVisual instance as input
+        public Worm(MyVisual mv) {
+            this.mv = mv;  //The passed MyVisual instance assigned to the local mv variable
+            w = mv.width + 16; // Width of the worm visualization adjusted to the width of the sketch
+            
+            // // Initialize worm parameters
+            for (int i = 0; i < maxworms; i++) {
+                amplitude[i] = mv.random(100,120); // Random amplitudes set for each wave
+                float period = mv.random(300,1000); // Random period set for each wave
+                dx[i] = (mv.TWO_PI / period) * xspacing;
+            } // End for
+            yvalues = new float[w/xspacing];    // Num of vertical locations based on the width and xspacing
+        } // End public worm()
 }
