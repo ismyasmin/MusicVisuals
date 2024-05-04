cb96a9a39aba40047607c357b2c019074722dd68
Yasmin Ismail <C22485282@mytudublin.ie>
2024-05-04 21:33:32 +0100

    render method to render the worm visualization. calcWorm calculates worm height values and computes worm heights for multiple worms combined together.

--- java/src/example/Worm.java
+++ java/src/example/Worm.java
@@ -7,7 +7,7 @@ public class Worm {
         int w;              // Width of entire worm
         int maxworms = 2;   // Amount of worms to add together
     
-        float theta = 0.0f; // Angle initialized for wave oscillation
+        float theta = 0.0f; // Angle initialized for worm oscillation
         float[] amplitude = new float[maxworms];   // Stores height of worm
         float[] dx = new float[maxworms];          // Value for incrementing X
         float[] yvalues;                           // Stores height values of the worm 
@@ -19,10 +19,35 @@ public class Worm {
             
             // // Initialize worm parameters
             for (int i = 0; i < maxworms; i++) {
-                amplitude[i] = mv.random(100,120); // Random amplitudes set for each wave
-                float period = mv.random(300,1000); // Random period set for each wave
+                amplitude[i] = mv.random(100,120); // Random amplitudes set for each worm
+                float period = mv.random(300,1000); // Random period set for each worm
                 dx[i] = (mv.TWO_PI / period) * xspacing;
             } // End for
             yvalues = new float[w/xspacing];    // Num of vertical locations based on the width and xspacing
         } // End public worm()
+        
+
+    public void render() {
+        mv.hint(mv.DISABLE_DEPTH_TEST);
+        calcWorm();
+        renderWorm();
+        mv.hint(mv.ENABLE_DEPTH_TEST);
+    }
+    void calcWorm() {
+        
+        theta += 0.02;
+
+        
+        for (int i = 0; i < yvalues.length; i++) {
+            float x = theta + (i * dx[0] / xspacing);
+            float wormHeight = 0;
+            for (int j = 0; j < maxworms; j++) {
+            
+                if (j % 2 == 0)  wormHeight += mv.sin(x)*amplitude[j];
+                else wormHeight += mv.cos(x)*amplitude[j];
+                x+=dx[j];
+            }
+            yvalues[i] = wormHeight;
+        }
+    }
 }
