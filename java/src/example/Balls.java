586815ae9b59f4255a11bb4451f9ed832980c7c5
Yasmin Ismail <C22485282@mytudublin.ie>
2024-05-06 00:43:36 +0100

    Random values assigned to amplitude and dx arrays for each ball, determining their properties. yvalues  initialized to store vertical positions of the balls  and spacing.

--- java/src/example/Balls.java
+++ java/src/example/Balls.java
@@ -16,5 +16,14 @@ public class Balls {
     public Balls(MyVisual mv) {
         this.mv = mv;  //The passed MyVisual instance assigned to the local mv variable
          w = mv.width + 16; // Width of the ball visualization adjusted to the width of the sketch
+
+           
+        // // Initialize ball parameters
+        for (int i = 0; i < maxballs; i++) {
+            amplitude[i] = mv.random(100,120); // Random amplitudes set for each ball
+            float period = mv.random(300,1000); // Random period set for each ball
+            dx[i] = (mv.TWO_PI / period) * xspacing;
+            } // End for
+            yvalues = new float[w/xspacing];    // Num of vertical locations based on the width and xspacing
             
 }
