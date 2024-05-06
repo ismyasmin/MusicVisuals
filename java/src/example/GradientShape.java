fe412cb0c1a7165737eadd34e09974a31464aebd
Yasmin Ismail <C22485282@mytudublin.ie>
2024-05-06 02:49:22 +0100

    Constructor Gradient(MyVisual mv)  initializes an instance of Gradient by associating it with a provided MyVisual object (mv), copies the canvas dimensions from mv, and calculating the size of gradient circles (dim).

--- java/src/example/GradientShape.java
+++ java/src/example/GradientShape.java
@@ -6,5 +6,12 @@ public class GradientShape {
     int dim; // Size of the gradient circles
     int height, width; // Dimensions of the canvas
     float audioAmplitude; // Stores the audio amplitude
+
+    public GradientShape(MyVisual mv) {
+        this.mv = mv;
+        this.height = mv.height;
+        this.width = mv.width;
+        dim = width / 2;
+    }
     
 }
